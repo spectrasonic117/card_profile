c70ba7b2a84a90dac759231b8afe2bc69e8d2a3d
Manuel Heredia <Spectrasonic117@gmail.com>
2024-09-12 14:31:24 -0600

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://spectrasonic117.github.io',
-	base: 'base_card'
+	site: 'https://spectrasonic117.github.io'
 });
