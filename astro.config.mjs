59332944581b693885d41decb5346bcaa4fe6eb0
Manuel Heredia <Spectrasonic117@gmail.com>
2024-09-12 15:14:48 -0600

    Update Astro.config.mjs to remove unused 'base' property

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 const LIVE_URL = 'https://spectrasonic117.github.io';
-const BASE = './dist';
 export default defineConfig({
 	integrations: [tailwind()],
-	site: LIVE_URL,
-	base: BASE
+	site: LIVE_URL
 });
